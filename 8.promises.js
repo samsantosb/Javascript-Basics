7e82b0aa37d3c874d0370ada08332240c26ab603
Samuel <80002045+samsantosb@users.noreply.github.com>
2023-10-20 19:51:00 -0300

    Update 8.promises.js

--- 8.promises.js
+++ 8.promises.js
@@ -56,22 +56,6 @@ async function executePromisesForAwait() {
 
 //executePromisesForAwait()
 
-//when to use for await
-//for await is used when you want to execute promises in sequence
-//so you can log the result of each promise as it is resolved
-
-//Promise All is used when you want to execute promises in parallel
-//it is faster than for await, but you can't log the result
-//of each promise before executing the next one
-//if one of the promises fails, the whole promise.all fails
-
-//Promise AllSettled is used when you want to execute promises in parallel
-//it is faster than for await, but you can't log the result
-//if one of the promises fails, the whole promise.allSettled doesn't fail
-
-//async await is used when you want execute one promise
-//.then is used when you want to execute one promise
-
 function executePromisesThen() {
     getPromise3().then((result) => {
         console.log(result);
